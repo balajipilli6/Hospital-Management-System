8fce7327a5875498d759dbad777d984fd3c4384b
balajipilli6 <balajipilli6@gmail.com>
2024-02-07 14:56:45 +0530

    Third commit

--- Hospital Management System/src/HelloWorld.java
+++ Hospital Management System/src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 public static void main(String[] args) {
 	System.out.println("hello world");
 	System.out.println("this class is abpout how to push the code local repository to remote repository");
+	System.out.println("hi, this is balaji ");
 }
 }
